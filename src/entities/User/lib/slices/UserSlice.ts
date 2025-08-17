84e3fc4a206a74ce70c0c026eddcb51765d72c90
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-08-17 18:12:31 +0300

    feat: VK ID

--- src/entities/User/lib/slices/UserSlice.ts
+++ src/entities/User/lib/slices/UserSlice.ts
@@ -43,12 +43,11 @@ export const handleCheckUser = createAsyncThunk<T_User, void, AsyncThunkConfig>(
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get("code");
         const deviceId = urlParams.get("device_id");
+        console.log(code);
+        console.log(deviceId);
 
         try {
-            console.log(data);
-            if (data) {
-                api.post("/auth/vk", { code, deviceId });
-            }
+            api.post("/auth/vk", { code, deviceId });
         } catch (e) {
             console.log("error");
             console.log(e);
